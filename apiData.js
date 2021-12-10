85128789bb75d284a87eb3716ac2b47591737a8d
Ayman <aymanab1971@gmail.com>
2021-12-10 10:59:49 +0100

    changes

--- apiData.js
+++ apiData.js
@@ -3,30 +3,32 @@ import axios from 'https://cdn.skypack.dev/axios';
 
 async function fetchWeatherData(url) {
     try {
-      let res = await axios({
-        url: url,
-        method: "get",
-        timeout: 8000,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (res.status == 200) {
-        // console.log(res.status)
-      }
-      console.log(res.data)
-      return res.data;
+        let res = await axios({
+            url: url,
+            method: "get",
+            timeout: 8000,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (res.status == 200) {
+            // console.log(res.status)
+        }
+        console.log(res.data)
+        return res.data;
     } catch (err) {
-      console.error(err);
+        console.error(err);
     }
-  }
+}
 
 const urlNewsApi = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
 
 
 fetchWeatherData(urlNewsApi)
 
-async function aymen() {
-    const apiData = await fetchWeatherData()
-    
+async function ayman() {
+    const apiData = await fetchWeatherData();
+    document.getElementById('content').innerHTML = `<p>${apiData.length}</p>`;
+
 }
+window.onload = ayman();
